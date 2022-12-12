917f8647b76e2ac67026ca4e9f87bfc77ee77b5f
Mawiz <mawiztech@gmaile.com>
2022-12-12 04:37:27 -0800

    printing

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - print numbers using putchar
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char 1;
+
+	for (1 = 1 ; 1 < 10 ; 1++)
+	{
+	putchar(1);
+	}
+	putchar('\n');
+	return (0);
+}
+	
