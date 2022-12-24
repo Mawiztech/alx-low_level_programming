c1f7d353e7815f3d04868ac3970c698373a0bce6
Mawiz <mawiztech@gmaile.com>
2022-12-24 12:28:32 -0800

    array pointers

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+
+
+/**
+ *
+ *_strncat - Concatenates two strings using @ bytes
+ *
+ *
+ *
+ *@dest: Destination String; src string is appended to it
+ *
+ *
+ *
+ *@src: Source string; gets appended to dest string
+ *
+ * 
+ * @n: Number of bytes of src that should be concatenated with dest
+ *
+ *
+ * 
+ *Return: Pointer to des
+
+ 
