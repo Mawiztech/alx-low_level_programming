219fdece736a0ab495e4a10edbff7e4e09942589
Mawiz <mawiztech@gmaile.com>
2022-12-29 05:54:38 -0800

    pointers

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -12,3 +12,5 @@ char *rot13(char *str);
 void print_number(int n);
 int _putchar(char c);
 void print_buffer(char *b, int size);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+#endif /*MAIN_H*/;
