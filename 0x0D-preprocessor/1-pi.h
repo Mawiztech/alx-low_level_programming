6d5f052dd3010191fba44d4ec05a45f051edae96
Mawiz <mawiztechnology@gmail.com>
2023-01-21 06:40:38 -0800

    Macrons

---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,17 @@
+#ifndef MACRO_H
+
+#define MACRO_H
+
+/**
+ *File: 1-pi
+ *
+ *
+ *Desc: header file
+ *
+ */
+
+#define PI 3.14159265359
+
+
+
+#endif
---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,20 @@
+fndef LIKEMACRO_H
+
+#define LIKEMACRO_H
+
+/**
+ * File: 4-sum
+ *
+ *@x: input
+ *
+ *@y: input
+ *
+ *Desc: header file
+ *
+ */
+
+#define SUM(x, y) (x + y)
+
+
+
+#endif
