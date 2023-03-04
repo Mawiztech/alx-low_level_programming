c03d26dfc4462e5624f276d45ac948f0e4608048
Mawiz <mawiztechnology@gmail.com>
2023-03-03 17:52:16 -0800

    files

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *  * error_file - checks if files can be opened.
+ *   * @file_from: file_from.
+ *    * @file_to: file_to.
+ *     * @argv: arguments vector.
+ *      * Return: no return.
+ *       */
+void error_file(int file_from, int file_to, char *argv[])
+{
